c6cc531a40736c6084a005fe2f8dffc301ce164b
gcoro <giulia994@ymail.com>
2020-03-12 20:18:16 +0100

    try to order by same company
    Co-authored-by: Trexy94 <trexy94@live.it>

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function parseInput(contentToParse) {
 	if (lines[lines.length - 1] === '')
 		lines.splice(lines.length - 1, 1);
 	const [v, h] = lines.splice(0, 1)[0].split(' ');
-	const map = lines.splice(0, +h);
+	const map = lines.splice(0, +h).map(line => line.split(''));
 	const developersCount = +lines.splice(0, 1)
 	const initialDevelopers = lines.splice(0, developersCount).map((developer, index) => {
 		const args = developer.split(" ");
@@ -39,24 +39,83 @@ function parseInput(contentToParse) {
 	return { initialManagers, initialDevelopers, map }
 }
 
+function tryToFill(company, i, j, map) {
+	if ((!company.managers.length && !company.developers.length) || map[i][j] === "#" || map[i][j] === 'O')
+		return
+
+	//developers[devCount].placement = { h: i, v: j };
+	if (map[i][j] === "M" && company.managers.length > 0) {
+		company.managers.pop().placement = { h: i, v: j };
+		map[i][j] = 'O'
+	} else if (map[i][j] === "_" && company.developers.length > 0) {
+		company.developers.pop().placement = { h: i, v: j };
+		map[i][j] = 'O'
+	} else return;
+
+	if (map[i + 1] && (map[i + 1][j] === "M" || map[i + 1][j] === "_"))
+		tryToFill(company, i + 1, j, map)
+	if (map[i][j + 1] === "M" || map[i][j + 1] === "_")
+		tryToFill(company, i, j + 1, map)
+	if (map[i - 1] && (map[i - 1][j] === "M" || map[i - 1][j] === "_"))
+		tryToFill(company, i - 1, j, map)
+	if (map[i][j - 1] === "M" || map[i][j - 1] === "_")
+		tryToFill(company, i, j - 1, map)
+}
+
 function compute(map, developers, managers) {
+
 	const hLength = map.length;
 	const vLength = map[0].length;
 
-	let devCount = 0;
-	let manCount = 0;
+	const companiesMap = {
+
+	}
+
+	developers.forEach(developer => {
+		if (!companiesMap[developer.company]) {
+			companiesMap[developer.company] = {
+				developers: [],
+				managers: []
+			}
+		}
+
+		companiesMap[developer.company].developers.push(developer)
+	})
+
+	managers.forEach(manager => {
+		if (!companiesMap[manager.company]) {
+			companiesMap[manager.company] = {
+				developers: [],
+				managers: []
+			}
+		}
+
+
+		companiesMap[manager.company].managers.push(manager)
+	})
 
 	for (let i = 0; i < hLength; i++) {
 		for (let j = 0; j < vLength; j++) {
-			if (manCount === managers.length && devCount === developers.length) break;
 
-			// random placement
+			/* random placement
+			if (manCount === managers.length && devCount === developers.length) break;
 			if (map[i][j] === '_' && devCount < developers.length) {
 				developers[devCount].placement = { h: i, v: j };
 				devCount++;
 			} else if (map[i][j] === 'M' && manCount < managers.length) {
 				managers[manCount].placement = { h: i, v: j };
-				manCount++;
+				manCount++; 
+			}*/
+
+			// keep near same company peeps
+			if (map[i][j] === '_' || map[i][j] === 'M') {
+				// place developers
+				const company = Object.keys(companiesMap)[0];
+				if (!company)
+					break;
+				tryToFill(companiesMap[company], i, j, map);
+				if (companiesMap[company].developers.length === 0)
+					delete companiesMap[company];
 			}
 		}
 	}
@@ -68,11 +127,11 @@ function parseOutput(developers, managers) {
 	const results = [];
 	// todo ? order developers by id
 	developers.forEach(el => {
-		results.push(el.placement ? `${el.placement.h} ${el.placement.v}` : 'X')
+		results.push(el.placement ? `${el.placement.v} ${el.placement.h}` : 'X')
 	});
 
 	managers.forEach(el => {
-		results.push(el.placement ? `${el.placement.h} ${el.placement.v}` : 'X')
+		results.push(el.placement ? `${el.placement.v} ${el.placement.h}` : 'X')
 	});
 
 	return results;
